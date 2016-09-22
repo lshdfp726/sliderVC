b63c6051bad4a8342090a1f7217aae1fc83f8368
lsh726 <yunxiyaoyue@163.com>
2016-09-22 11:12:43 +0800

    重写

--- ManyViewSlider/SliderModule/VC/PageViewController.h
+++ ManyViewSlider/SliderModule/VC/PageViewController.h
@@ -1,13 +1,13 @@
 //
-//  SliderViewController.h
+//  PageViewController.h
 //  ManyViewSlider
 //
-//  Created by 刘松洪 on 16/9/8.
+//  Created by 刘松洪 on 16/9/22.
 //  Copyright © 2016年 刘松洪. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SliderViewController : UIViewController
+@interface PageViewController : UIViewController
 
 @end
---
+++ ManyViewSlider/SliderModule/VC/PageViewController.m
@@ -0,0 +1,37 @@
+//
+//  PageViewController.m
+//  ManyViewSlider
+//
+//  Created by 刘松洪 on 16/9/22.
+//  Copyright © 2016年 刘松洪. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+@interface PageViewController ()
+
+@end
+
+@implementation PageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
